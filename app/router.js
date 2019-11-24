51111ebaf5452d5dc8d955b83057791167a23106
Roel de Jong <Roel4811@hotmail.com>
2019-11-25 00:03:23 +0100

    update router syntax

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
 
 Router.map(function() {
   this.route('about');
@@ -12,3 +12,5 @@ Router.map(function() {
     this.route('show');
   });
 });
+
+export default Router;
